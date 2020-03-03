6a296f830c00fd69278a38c6f9a043c3d56cbd3d
Jeslas Pravin <pravinjeslas@gmail.com>
2020-03-03 17:30:32 +0100

    Added basic memory resources

--- Source/EngineMain/RenderInterface/Resources/MemoryResources.h
+++ Source/EngineMain/RenderInterface/Resources/MemoryResources.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "GraphicsResources.h"
 #include "../../Core/Platform/PlatformTypes.h"
-
+#include <glm/detail/type_vec3.hpp>
 
 class MemoryResource : public GraphicsResource
 {
@@ -13,4 +13,22 @@ public:
 class BufferResource : public MemoryResource
 {
     DECLARE_GRAPHICS_RESOURCE(BufferResource, , MemoryResource, )
+};
+
+namespace EImageDataFormat
+{
+    enum Type
+    {
+
+    };
+}
+class ImageResource : public MemoryResource
+{
+    DECLARE_GRAPHICS_RESOURCE(ImageResource, , MemoryResource, )
+
+protected:
+    EImageDataFormat::Type dataFormat;
+    uint32 numOfMips = 0;
+    glm::vec<3,uint32, glm::defaultp> dimensions = { 256,256,1 };
+    uint32 layerCount = 1;
 };
