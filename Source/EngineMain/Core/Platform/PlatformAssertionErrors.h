4394d5bbbab692505bf316a4dd36f4adc21b6c34
Jeslas Pravin <pravinjeslas@gmail.com>
2020-03-28 20:09:27 +0100

    Few small fixes and changes

--- Source/EngineMain/Core/Platform/PlatformAssertionErrors.h
+++ Source/EngineMain/Core/Platform/PlatformAssertionErrors.h
@@ -9,3 +9,24 @@ public:
     virtual void unregisterFilter() = 0;
     virtual void dumpCallStack() = 0;
 };
+
+#if _DEBUG
+#ifndef dbgAssert
+#define dbgAssert(Expr)\
+if(!(Expr)){\
+    Logger::error("DebugAssertion", "%s() : Assert expression failed "#Expr,__func__);\
+    UnexpectedErrorHandler::getHandler()->dumpCallStack();}
+#endif
+#else
+#ifndef dbgAssert
+#define dbgAssert(Expr)\
+if((Expr)){}
+#endif
+#endif
+
+#ifndef fatalAssert
+#define fatalAssert(Expr,Message)\
+if(!(Expr)){\
+    Logger::error("DebugAssertion", "%s() : Assert expression failed "#Expr"[%s]",__func__,Message);\
+    UnexpectedErrorHandler::getHandler()->dumpCallStack();}
+#endif
