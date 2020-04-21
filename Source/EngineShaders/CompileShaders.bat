9a77c6cfdfae35794fc304668beca2fea07b2ed5
Jeslas Pravin <pravinjeslas@gmail.com>
2020-04-21 16:28:05 +0200

    Codes to draw first triangle of the engine

---
+++ Source/EngineShaders/CompileShaders.bat
@@ -0,0 +1,4 @@
+D:
+cd D:/Workspaces/Visual Studio 2017/GameEngine/Binaries/GameBuilder
+GameBuilder.exe -mode shaderCompile -targetPath "D:/Workspaces/Visual Studio 2017/GameEngine/Binaries/EngineMain/x64/Debug" -api vulkan
+GameBuilder.exe -mode shaderCompile -targetPath "D:/Workspaces/Visual Studio 2017/GameEngine/Binaries/EngineMain/x64/Release" -api vulkan
--- Source/EngineShaders/Utilities/DrawQuadFromTexture.frag.glsl
+++ Source/EngineShaders/Utilities/DrawQuadFromTexture.frag.glsl
@@ -4,6 +4,7 @@ layout(location = 0) in vec2 inTextureCoord;
 
 layout(location = 0) out vec4 colorAttachment0;
 
+// TODO(Jeslas) : Update bindings and set once implementations become more concrete
 layout(set = 0, binding = 2) uniform sampler textureSampler;
 layout(set = 0, binding = 3) uniform texture2D quadTexture;
 
