2b29bb5163eede70d3b5c4b1f0fd6c9d8b0c4615
Jeslas Pravin <pravinjeslas@gmail.com>
2020-04-21 16:24:19 +0200

    Added shader compiling to GameBuilder

---
+++ GameBuilder/Modes/CompileShadersMode.cs
@@ -0,0 +1,50 @@
+﻿using GameBuilder.ShaderCompiling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBuilder.Modes
+{
+    enum ReleaseConfigurations
+    {
+        Release,
+        Debug
+    }
+    class CompileShadersMode : ProgramMode
+    {
+        private const string RENDER_API = "api";
+        private const string COMPILER_DIRECTORY = "binPath";
+        private const string TARGET_DIR = "targetPath";
+
+        private const string API_VULKAN = "vulkan";
+        public override ModeExecutionResult execute(Dictionary<string,List<string>> consoleArgs)
+        {
+            if(consoleArgs.ContainsKey(RENDER_API) && consoleArgs.ContainsKey(TARGET_DIR))
+            {
+                ShaderCompiler compiler = null;
+                string compilerPath = null;
+                if(consoleArgs.ContainsKey(COMPILER_DIRECTORY))
+                {
+                    compilerPath = consoleArgs[COMPILER_DIRECTORY][0];
+                }
+
+                switch(consoleArgs[RENDER_API][0])
+                {
+                    case API_VULKAN:
+                        compiler = new VulkanShaderCompiler(compilerPath, consoleArgs[TARGET_DIR][0]);
+                        break;
+                    default:
+                        return ModeExecutionResult.Failure;
+                }
+
+                if(compiler.compile(consoleArgs))
+                {
+                    return ModeExecutionResult.Success;
+                }
+            }
+            return ModeExecutionResult.Failure;
+        }
+    }
+}
