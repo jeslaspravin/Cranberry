e2af70dfde37f499efe95393aca77eb0090cdf04
Jeslas Pravin <pravinjeslas@gmail.com>
2020-09-28 20:42:01 +0200

    Added Gooch shading model

---
+++ Source/EngineMain/RenderInterface/Shaders/EngineShaders/GoochModel.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "../../../Core/Math/Vector3D.h"
+#include "../../../Core/Math/Vector4D.h"
+
+
+struct SurfaceData
+{
+    Vector3D lightPos;
+    Vector4D highlightColor;
+    Vector4D surfaceColor;
+};
