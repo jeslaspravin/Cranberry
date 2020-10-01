7072be60910f71076618c9d8a378d11eebe5a800
Jeslas Pravin <pravinjeslas@gmail.com>
2020-10-01 14:10:01 +0200

    Few small fixes and improvements

--- Source/EngineShaders/Default/DefaultStaticMeshMultibuffer.vert.glsl
+++ Source/EngineShaders/Default/DefaultStaticMeshMultibuffer.vert.glsl
@@ -15,9 +15,12 @@
 void mainVS()
 {
     vec4 worldPos = instanceData.model * vec4(position.xyz, 1);
-    vec4 clipPos = viewData.projection * viewData.invView * worldPos;
-    outWorldPosition = vec4(worldPos.xyz, clipPos.z);
-    gl_Position = clipPos;
+    vec4 viewPos = viewData.invView * worldPos;
+    float f = - viewData.projection[2][3] / viewData.projection[2][2];
+    float n = viewData.projection[2][3] / (1 - viewData.projection[2][2]);
+    outWorldPosition = vec4(worldPos.xyz, 1 - viewPos.z/(f - n));
+//    outWorldPosition = vec4(worldPos.xyz, viewPos.z);
+    gl_Position = viewData.projection * viewPos;
     outLocalPosition = position.xyz;
     outWorldNormal = (transpose(instanceData.invModel) * vec4(normal.xyz, 1)).xyz;
     outLocalNormal = normal.xyz;
