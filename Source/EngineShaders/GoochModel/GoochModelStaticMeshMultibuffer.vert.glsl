7eb85f437fd3fe4214a1eb25a1ba637bdadbc998
Jeslas Pravin <pravinjeslas@gmail.com>
2020-10-01 14:10:01 +0200

    Few small fixes and improvements

--- Source/EngineShaders/GoochModel/GoochModelStaticMeshMultibuffer.vert.glsl
+++ Source/EngineShaders/GoochModel/GoochModelStaticMeshMultibuffer.vert.glsl
@@ -22,14 +22,17 @@ layout(set = 2, binding = 0) uniform SurfaceData
 void mainVS()
 {
     vec4 worldPos = instanceData.model * vec4(position.xyz, 1);
-    vec4 clipPos = viewData.projection * viewData.invView * worldPos;
-    outWorldPosition = vec4(worldPos.xyz, clipPos.z/clipPos.w);
-    gl_Position = clipPos;
+    vec4 viewPos = viewData.invView * worldPos;
+    float f = - viewData.projection[2][3] / viewData.projection[2][2];
+    float n = viewData.projection[2][3] / (1 - viewData.projection[2][2]);
+    outWorldPosition = vec4(worldPos.xyz, 1 - viewPos.z/(f - n));
+//    outWorldPosition = vec4(worldPos.xyz, viewPos.z);
+    gl_Position = viewData.projection * viewPos;
     outWorldNormal = (transpose(instanceData.invModel) * vec4(normal.xyz, 1)).xyz;
 
     vec3 lightDir = normalize(surfaceData.lightPos - worldPos.xyz);
     float colorCoeff =  (dot(outWorldNormal, lightDir) + 1) * 0.5;
-    float highlightCoeff = clamp(100 * dot((2 * dot(outWorldNormal, lightDir) * outWorldNormal - lightDir), viewData.view[2].xyz) - 97, 0.0, 1.0);
+    float highlightCoeff = clamp((100 * dot((2 * dot(outWorldNormal, lightDir) * outWorldNormal - lightDir), viewData.view[2].xyz) - 97) * 0.333333, 0.0, 1.0);
     outColor = highlightCoeff * surfaceData.highlightColor + (1 - highlightCoeff) * (
         colorCoeff * vec4(vec3(0.3, 0.3, 0.0) + 0.25 * surfaceData.surfaceColor.xyz, surfaceData.surfaceColor.w) 
         + (1 - colorCoeff) * vec4(vec3(0, 0, 0.55) + 0.25 * surfaceData.surfaceColor.xyz, surfaceData.surfaceColor.w));
