05da0fe461704ed5acd1a768a5388ea30657fdbf
Jeslas Pravin <pravinjeslas@gmail.com>
2021-01-01 14:10:50 +0100

    refactor: Added ReleaseExperimental configuration, Changes to support new config in batch files, Changes time resolution from milli to micro seconds as in release delta time is too low

--- Source/EngineShaders/CompileShaders.bat
+++ Source/EngineShaders/CompileShaders.bat
@@ -15,11 +15,20 @@ pushd %binariesPath%\GameBuilder
 echo EXE : GameBuilder.exe -mode shaderCompile -targetPath "%binariesPath%/EngineMain/x64/Debug" -intermediatePath "%binariesPath%/EngineMain/x64/Debug/Intermediate" -api vulkan
 GameBuilder.exe -mode shaderCompile -targetPath "%binariesPath%/EngineMain/x64/Debug" -intermediatePath "%binariesPath%/EngineMain/x64/Debug/Intermediate" -api vulkan
 
-echo EXE : GameBuilder.exe -mode shaderCompile -targetPath "%binariesPath%/EngineMain/x64/EXPERIMENTAL" -intermediatePath "%binariesPath%/EngineMain/x64/EXPERIMENTAL/Intermediate" -api vulkan
-GameBuilder.exe -mode shaderCompile -targetPath "%binariesPath%/EngineMain/x64/EXPERIMENTAL" -intermediatePath "%binariesPath%/EngineMain/x64/EXPERIMENTAL/Intermediate" -api vulkan
-
 echo EXE : GameBuilder.exe -mode shaderCompile -targetPath "%binariesPath%/EngineMain/x64/Release" -intermediatePath "%binariesPath%/EngineMain/x64/Release/Intermediate" -api vulkan
 GameBuilder.exe -mode shaderCompile -targetPath "%binariesPath%/EngineMain/x64/Release" -intermediatePath "%binariesPath%/EngineMain/x64/Release/Intermediate" -api vulkan
+
+rem Just copying as there is not change btw non experimental and experimental right now
+rmdir /s /q "%binariesPath%/EngineMain/x64/ReleaseExperimental/Shaders"
+xcopy /s /i /f "%binariesPath%/EngineMain/x64/Release/Shaders" "%binariesPath%/EngineMain/x64/ReleaseExperimental/Shaders"
+rem echo EXE : GameBuilder.exe -mode shaderCompile -targetPath "%binariesPath%/EngineMain/x64/ReleaseExperimental" -intermediatePath "%binariesPath%/EngineMain/x64/ReleaseExperimental/Intermediate" -api vulkan
+rem GameBuilder.exe -mode shaderCompile -targetPath "%binariesPath%/EngineMain/x64/ReleaseExperimental" -intermediatePath "%binariesPath%/EngineMain/x64/ReleaseExperimental/Intermediate" -api vulkan
+
+rem Just copying as there is not change btw non experimental and experimental right now
+rmdir /s /q "%binariesPath%/EngineMain/x64/DebugExperimental/Shaders"
+xcopy /s /i /f "%binariesPath%/EngineMain/x64/Debug/Shaders" "%binariesPath%/EngineMain/x64/DebugExperimental/Shaders"
+rem echo EXE : GameBuilder.exe -mode shaderCompile -targetPath "%binariesPath%/EngineMain/x64/DebugExperimental" -intermediatePath "%binariesPath%/EngineMain/x64/DebugExperimental/Intermediate" -api vulkan
+rem GameBuilder.exe -mode shaderCompile -targetPath "%binariesPath%/EngineMain/x64/DebugExperimental" -intermediatePath "%binariesPath%/EngineMain/x64/DebugExperimental/Intermediate" -api vulkan
 
 popd
 
