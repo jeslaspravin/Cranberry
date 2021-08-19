630d508fbcbce007f3cc13d8e426b10b648cfec0
Jeslas Pravin <pravinjeslas@gmail.com>
2021-08-19 20:29:57 +0200

    Improved shader compilation cache

--- Source/EngineShaders/CompileShaders.bat
+++ Source/EngineShaders/CompileShaders.bat
@@ -19,16 +19,16 @@ echo EXE : GameBuilder.exe -mode shaderCompile -targetPath "%binariesPath%/Engin
 GameBuilder.exe -mode shaderCompile -targetPath "%binariesPath%/EngineMain/x64/Release" -intermediatePath "%binariesPath%/EngineMain/x64/Release/Intermediate" -api vulkan
 
 rem Just copying as there is not change btw non experimental and experimental right now
-rmdir /s /q "%binariesPath%/EngineMain/x64/ReleaseExperimental/Shaders"
-xcopy /s /i /f "%binariesPath%/EngineMain/x64/Release/Shaders" "%binariesPath%/EngineMain/x64/ReleaseExperimental/Shaders"
-rem echo EXE : GameBuilder.exe -mode shaderCompile -targetPath "%binariesPath%/EngineMain/x64/ReleaseExperimental" -intermediatePath "%binariesPath%/EngineMain/x64/ReleaseExperimental/Intermediate" -api vulkan
-rem GameBuilder.exe -mode shaderCompile -targetPath "%binariesPath%/EngineMain/x64/ReleaseExperimental" -intermediatePath "%binariesPath%/EngineMain/x64/ReleaseExperimental/Intermediate" -api vulkan
+REM rmdir /s /q "%binariesPath%/EngineMain/x64/ReleaseExperimental/Shaders"
+REM xcopy /s /i /f "%binariesPath%/EngineMain/x64/Release/Shaders" "%binariesPath%/EngineMain/x64/ReleaseExperimental/Shaders"
+echo EXE : GameBuilder.exe -mode shaderCompile -targetPath "%binariesPath%/EngineMain/x64/ReleaseExperimental" -intermediatePath "%binariesPath%/EngineMain/x64/ReleaseExperimental/Intermediate" -api vulkan
+GameBuilder.exe -mode shaderCompile -targetPath "%binariesPath%/EngineMain/x64/ReleaseExperimental" -intermediatePath "%binariesPath%/EngineMain/x64/ReleaseExperimental/Intermediate" -api vulkan
 
 rem Just copying as there is not change btw non experimental and experimental right now
-rmdir /s /q "%binariesPath%/EngineMain/x64/DebugExperimental/Shaders"
-xcopy /s /i /f "%binariesPath%/EngineMain/x64/Debug/Shaders" "%binariesPath%/EngineMain/x64/DebugExperimental/Shaders"
-rem echo EXE : GameBuilder.exe -mode shaderCompile -targetPath "%binariesPath%/EngineMain/x64/DebugExperimental" -intermediatePath "%binariesPath%/EngineMain/x64/DebugExperimental/Intermediate" -api vulkan
-rem GameBuilder.exe -mode shaderCompile -targetPath "%binariesPath%/EngineMain/x64/DebugExperimental" -intermediatePath "%binariesPath%/EngineMain/x64/DebugExperimental/Intermediate" -api vulkan
+REM rmdir /s /q "%binariesPath%/EngineMain/x64/DebugExperimental/Shaders"
+REM xcopy /s /i /f "%binariesPath%/EngineMain/x64/Debug/Shaders" "%binariesPath%/EngineMain/x64/DebugExperimental/Shaders"
+echo EXE : GameBuilder.exe -mode shaderCompile -targetPath "%binariesPath%/EngineMain/x64/DebugExperimental" -intermediatePath "%binariesPath%/EngineMain/x64/DebugExperimental/Intermediate" -api vulkan
+GameBuilder.exe -mode shaderCompile -targetPath "%binariesPath%/EngineMain/x64/DebugExperimental" -intermediatePath "%binariesPath%/EngineMain/x64/DebugExperimental/Intermediate" -api vulkan
 
 popd
 
