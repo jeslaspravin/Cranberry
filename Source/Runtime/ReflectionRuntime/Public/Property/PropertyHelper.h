1b24cbc7a310487a005b22f966212efcc54693b0
Jeslas Pravin <pravinjeslas@gmail.com>
2022-01-05 20:51:00 +0100

    Few minor changes

--- Source/Runtime/ReflectionRuntime/Public/Property/PropertyHelper.h
+++ Source/Runtime/ReflectionRuntime/Public/Property/PropertyHelper.h
@@ -8,9 +8,18 @@ class REFLECTIONRUNTIME_EXPORT PropertyHelper
 private:
     PropertyHelper() = default;
 public:
-    // Returns valid name that be used as name for function or class or other user symbol in C++. 
-    // Does not handles special key words in C++ so caller must ensure that resulting string does not leads to keyword
-    // Replaces any none allowed characters in symbol name to underscore
+    /**
+    * PropertyHelper::getValidSymbolName - 
+    * Returns valid name that be used as name for function or class or other user symbol in C++. 
+    * Does not handles special key words in C++ so caller must ensure that resulting string does not leads to keyword
+    * Replaces any none allowed characters in symbol name to underscore
+    * 
+    * Access: public static  
+    *
+    * @param const String & inValue 
+    *
+    * @return String
+    */
     static String getValidSymbolName(const String& inValue);
     static bool isValidSymbolName(const String& inValue);
     static bool isValidFunctionCall(const String& inValue);
