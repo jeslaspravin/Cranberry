b7599396fb090686556c03d842bd5d0bd3ab65b7
Jeslas Pravin <pravinjeslas@gmail.com>
2022-08-03 22:47:07 +0200

    Add CBEEditor
    Few modules made public in some modules
    Few fixes

---
+++ Source/Editor/CBEEditor/Private/CBEEditorModule.cpp
@@ -0,0 +1,14 @@
+/*!
+ * \file CBEEditorModule.cpp
+ *
+ * \author Jeslas
+ * \date August 2022
+ * \copyright
+ *  Copyright (C) Jeslas Pravin, Since 2022
+ *  @jeslaspravin pravinjeslas@gmail.com
+ *  License can be read in LICENSE file at this repository's root
+ */
+
+#include "Modules/ModuleManager.h"
+
+DECLARE_MODULE(CBEEditor, ModuleNoImpl)
