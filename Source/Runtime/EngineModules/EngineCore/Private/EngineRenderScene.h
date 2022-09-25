709dd0bb6928f3aaaa11b1bd527a89552f39be35
Jeslas Pravin <pravinjeslas@gmail.com>
2022-09-25 17:57:05 +0200

    Initial EngineRenderScene codes

--- Source/Runtime/EngineModules/EngineCore/Private/EngineRenderScene.h
+++ Source/Runtime/EngineModules/EngineCore/Private/EngineRenderScene.h
@@ -13,10 +13,11 @@
 
 #include "String/StringID.h"
 #include "Math/Box.h"
-#include "ObjectPtrs.h"
+#include "Memory/LinearAllocator.h"
 #include "Types/Containers/BitArray.h"
 #include "Types/Containers/SparseVector.h"
 #include "Types/Platform/Threading/CoPaT/JobSystemCoroutine.h"
+#include "ObjectPtrs.h"
 #include "Render/EngineRenderTypes.h"
 #include "RenderInterface/Resources/MemoryResources.h"
 #include "RenderInterface/ShaderCore/ShaderParameterResources.h"
@@ -102,6 +103,30 @@ public:
     const RendererIntermTexture *getTexture(IRenderCommandList *cmdList, ERendererIntermTexture::Type rtType, Size2D size);
 
     void clearUnused(IRenderCommandList *cmdList);
+
+    // Waits and clears the entire texture pool
+    void clearPool(IRenderCommandList *cmdList);
+};
+
+// This component is temporary and might change to something better later
+struct ComponentRenderInfo
+{
+    // materialIndex is just a cache. 0 is invalid
+    SizeT materialIndex;
+    String shaderName;
+    StringID materialID;
+
+    // TODO(Jeslas) : Will be changed after other vertex types of meshes are added
+    // 0 is invalid
+    Transform3D worldTf;
+    SizeT tfIndex = 0;
+
+    // vertexBuffers[vertexType].meshes[meshID] gives vertex information for this component
+    EVertexType::Type vertexType;
+    StringID meshID;
+
+    BufferResourceRef cpuVertBuffer;
+    BufferResourceRef cpuIdxBuffer;
 };
 
 // 1:1 to each world
@@ -109,32 +134,104 @@ class EngineRenderScene
 {
 private:
     constexpr static const uint32 BUFFER_COUNT = 2;
-    struct ComponentRenderInfo
+    constexpr static const uint32 VERTEX_TYPE_COUNT = EVertexType::TypeEnd - EVertexType::TypeStart;
+
+    using RenderInfoVector = SparseVector<ComponentRenderInfo, BitArraySparsityPolicy>;
+    static_assert(std::is_same_v<RenderInfoVector::size_type, SizeT>, "Component index type mismatch");
+
+    struct MeshVertexView
     {
-        EVertexType::Type vertexType;
-        SizeT tfIndex;
-        SizeT materialIndex;
+        // In counts not bytes
+        SizeT vertOffset;
+        SizeT vertCount = 0;
+
+        SizeT idxOffset;
+        SizeT idxCount = 0;
+
+        SizeT refs = 0;
+    };
+    struct VerticesPerVertType
+    {
+        BufferResourceRef vertices;
+        BufferResourceRef indices;
+
+        LinearAllocationTracker<1> vertsAllocTracker;
+        LinearAllocationTracker<1> idxsAllocTracker;
+        std::unordered_map<StringID, MeshVertexView> meshes;
+
+        // List of meshes and Component render info index to add for first time
+        std::vector<std::pair<StringID, SizeT>> meshesToAdd;
+        // List of meshes to remove in case vertex updates are in progress
+        std::vector<StringID> meshesToRemove;
+
+        // Batch copy all vertices
+        std::vector<BatchCopyBufferInfo> copies;
+    };
+    struct InstanceParamsPerVertType
+    {
+        BufferResourceRef instanceData;
+        LinearAllocationTracker<1> allocTracker;
+
+        ShaderParametersRef shaderParameter;
+
+        std::vector<SizeT> compIdxToAdd;
+        std::vector<SizeT> instanceIdxToRemove;
+
+        std::vector<BatchCopyBufferInfo> copies;
+        std::vector<BatchCopyBufferData> hostToBufferCopies;
     };
 
+    struct MaterialShaderParams
+    {
+        // Draw lists are for main GBuffer pass. For lights and other rendering passes separate draw list must be maintained
+        uint64 drawListCount[VERTEX_TYPE_COUNT];
+        BufferResourceRef drawListPerVertType[VERTEX_TYPE_COUNT];
+
+        BufferResourceRef materialData;
+        ShaderParametersRef shaderParameter;
+        LinearAllocationTracker<1> materialAllocTracker;
+        std::vector<uint32> materialRefs;
+        // Idx is not material idx but the direct vector idx. No need to do materialIdxToVectorIdx()
+        std::unordered_map<StringID, SizeT> materialToIdx;
+
+        std::vector<SizeT> compIdxToAdd;
+        std::vector<StringID> materialIDToRemove;
+
+        std::vector<BatchCopyBufferData> drawListCopies;
+        std::vector<BatchCopyBufferInfo> materialCopies;
+        std::vector<BatchCopyBufferData> hostToMatCopies;
+    };
+
+    uint64 frameCount = 0;
+
     cbe::ObjectPath world;
-    SparseVector<ComponentRenderInfo, BitArraySparsityPolicy> compsRenderInfo;
+    RenderInfoVector compsRenderInfo;
     std::unordered_map<StringID, SizeT> componentToRenderInfo;
+    ComponentRenderSyncInfo componentUpdates;
 
     SceneRenderTexturePool rtPool;
 
+    bool bVertexUpdating = false;
+    VerticesPerVertType vertexBuffers[VERTEX_TYPE_COUNT];
+
+    bool bMaterialsUpdating = false;
+    std::unordered_map<String, MaterialShaderParams> shaderToMaterials;
+
+    bool bInstanceParamsUpdating = false;
+    InstanceParamsPerVertType instancesData[VERTEX_TYPE_COUNT];
+
+    String getTransferCmdBufferName() const;
+    String getCmdBufferName() const;
+
     // Initial test code for RT pool
-    uint64 frameCount = 0;
     RendererIntermTexture lastRT;
     const RendererIntermTexture &getTempTexture(IRenderCommandList *cmdList, Short2D size);
-    String getCmdBufferName() const;
 
     ShaderParametersRef clearParams;
 
 public:
     EngineRenderScene(cbe::World *inWorld);
     MAKE_TYPE_NONCOPY_NONMOVE(EngineRenderScene)
-
-    copat::JobSystemEnqTask<copat::EJobThreadType::RenderThread> syncWorldComps(ComponentRenderSyncInfo compsUpdate);
 
     void clearScene();
 
@@ -149,9 +246,52 @@ public:
     void onLastRTCopied();
 
 private:
-    void createRenderInfo(cbe::RenderableComponent *comp, ComponentRenderInfo &outRenderInfo) const;
-    void destroyRenderInfo(const ComponentRenderInfo &renderInfo) const;
+    // Add/Remove mesh ref is used when creating or deleting a component in render scene to clear those vertices in scene vert and index buffers
+    FORCE_INLINE void addMeshRef(EVertexType::Type vertType, StringID meshID, SizeT compRenderInfoIdx);
+    FORCE_INLINE void removeMeshRef(EVertexType::Type vertType, MeshVertexView &meshVertView, StringID meshID);
 
+    FORCE_INLINE SizeT instanceIdxToVectorIdx(SizeT instanceIdx) const { return instanceIdx - 1; }
+    FORCE_INLINE SizeT vectorIdxToInstanceIdx(SizeT idx) const { return idx + 1; }
+    FORCE_INLINE void createInstanceCopies(
+        InstanceParamsPerVertType &vertInstanceData, const ComponentRenderInfo &compRenderInfo, IRenderCommandList *cmdList,
+        IGraphicsInstance *graphicsInstance
+    ) const;
+    FORCE_INLINE void addCompInstanceData(SizeT compRenderInfoIdx);
+    FORCE_INLINE void removeInstanceDataAt(EVertexType::Type vertexType, SizeT instanceIdx);
+
+    FORCE_INLINE SizeT materialIdxToVectorIdx(SizeT materialIdx) const { return materialIdx - 1; }
+    FORCE_INLINE SizeT vectorIdxToMaterialIdx(SizeT idx) const { return idx + 1; }
+    FORCE_INLINE void createMaterialCopies(
+        MaterialShaderParams &shaderMats, const ComponentRenderInfo &compRenderInfo, IRenderCommandList *cmdList,
+        IGraphicsInstance *graphicsInstance
+    ) const;
+    FORCE_INLINE void addCompMaterialData(SizeT compRenderInfoIdx);
+    // Why material vector index here alone? Because in materialIDToIdx each material ID directly maps to actual vector idx
+    // And only materialIDs to be removed is stored
+    FORCE_INLINE void removeMaterialAt(SizeT matVectorIdx, StringID materialID, MaterialShaderParams &shaderMats);
+
+    void addRenderComponents(const std::vector<cbe::RenderableComponent *> &renderComps);
+    void removeRenderComponents(const std::vector<StringID> &renderComps);
+    void recreateRenderComponents(const std::vector<cbe::RenderableComponent *> &renderComps);
+    void updateTfComponents(
+        const std::vector<cbe::TransformComponent *> &comps, IRenderCommandList *cmdList, IGraphicsInstance *graphicsInstance
+    );
+
+    void createRenderInfo(cbe::RenderableComponent *comp, SizeT compRenderInfoIdx);
+    void destroyRenderInfo(const cbe::RenderableComponent *comp, SizeT compRenderInfoIdx);
+
+    void syncWorldCompsRenderThread(
+        const ComponentRenderSyncInfo &compsUpdate, IRenderCommandList *cmdList, IGraphicsInstance *graphicsInstance,
+        const GraphicsHelperAPI *graphicsHelper
+    );
+    // Adds new mesh's vertex and index buffer, resizes the corresponding scene buffers to upload them
+    copat::NormalFuncAwaiter
+        recreateSceneVertexBuffers(IRenderCommandList *cmdList, IGraphicsInstance *graphicsInstance, const GraphicsHelperAPI *graphicsHelper);
+    copat::NormalFuncAwaiter
+        recreateMaterialBuffers(IRenderCommandList *cmdList, IGraphicsInstance *graphicsInstance, const GraphicsHelperAPI *graphicsHelper);
+    copat::NormalFuncAwaiter
+        recreateInstanceBuffers(IRenderCommandList *cmdList, IGraphicsInstance *graphicsInstance, const GraphicsHelperAPI *graphicsHelper);
+    void performTransferCopies(IRenderCommandList *cmdList, IGraphicsInstance *graphicsInstance, const GraphicsHelperAPI *graphicsHelper);
     void renderTheSceneRenderThread(
         Short2D viewportSize, const Camera &viewCamera, IRenderCommandList *cmdList, IGraphicsInstance *graphicsInstance,
         const GraphicsHelperAPI *graphicsHelper
